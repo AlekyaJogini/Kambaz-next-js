8f9d29277ae6dfb2dad3ae667ee118bc6d0de175
Alekya Jogini <alekyajogini@Alekyas-MacBook-Air.local>
2025-09-18 01:19:34 -0400

    deploment resolution 15

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -11,9 +11,8 @@ export default async function Layout({
   const { cid } = await params;
   
   return (
-    <div>
-      {/* You can use cid here if needed */}
+    
       {children}
-    </div>
+    
   );
 }
