f019e1707e3e8daf7c44c2d03072a62b836ff67d
Alekya Jogini <alekyajogini@Alekyas-MacBook-Air.local>
2025-11-21 23:50:52 -0500

    newlabchange

--- app/(Kambaz)/Labs/page.tsx
+++ app/(Kambaz)/Labs/page.tsx
@@ -21,6 +21,11 @@ export default function Labs() {
          <Link href="/Labs/Lab4" id="wd-lab4-link">
            Lab 4: React Applications </Link>
        </li>
+
+       <li>
+         <Link href="/Labs/Lab4" id="wd-lab4-link">
+           Lab 5:  Applications </Link>
+       </li>
       
        
 
